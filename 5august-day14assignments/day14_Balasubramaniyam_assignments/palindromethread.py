d3d1003cc73b22a3bcce1e6c1fa93fd61d17055d
Balu862 <balusaravanan862@gmail.com>
2021-08-06 10:19:40 +0530

    Balu14 reassigned

--- 5august-day14assignments/day14_Balasubramaniyam_assignments/palindromethread.py
+++ 5august-day14assignments/day14_Balasubramaniyam_assignments/palindromethread.py
@@ -1,24 +1,31 @@
-import time,threading
-def prime():
-    for i in range(2,500):
-        t=0
-        for j in range(2,i):
-            if i%j==0:
-                t=1
+import time,threading,logging
+logging.basicConfig(filename='thread1.log',level=logging.DEBUG)
+try:
+    
+    def prime():
+        for i in range(2,500):
+            t=0
+            for j in range(2,i):
+                if i%j==0:
+                    t=1
+                    #time.sleep(1)
+            if t==0:
+                print("prime",i)
                 #time.sleep(1)
-        if t==0:
-            print("prime",i)
-            #time.sleep(1)
-def palindrome():
-    for i in range(2,500):
-        a=str(i)
-        if a==a[::-1]:
-            print("palindrome",i)
-            
-
-t1=threading.Thread(target=prime)
-t2=threading.Thread(target=palindrome)
-t1.start()
-t2.start()
-t1.join()
-t2.join()
+        logging.info("prime is completed")
+    def palindrome():
+        for i in range(2,500):
+            a=str(i)
+            if a==a[::-1]:
+                print("palindrome",i)
+                
+        logging.info("palindrome is completed")
+    if __name__ == "__main__":
+        t1=threading.Thread(target=prime)
+        t2=threading.Thread(target=palindrome)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+except:
+    logging.error("error is occured")
--- 5august-day14assignments/day14_Balasubramaniyam_assignments/primepalindromeprocess.py
+++ 5august-day14assignments/day14_Balasubramaniyam_assignments/primepalindromeprocess.py
@@ -1,24 +1,31 @@
-import time,multiprocessing
-def prime():
-    for i in range(2,500):
-        t=0
-        for j in range(2,i):
-            if i%j==0:
-                t=1
+import time,multiprocessing,logging
+logging.basicConfig(filename='multiprocess.log',level=logging.DEBUG)
+try:
+
+    def prime():
+        for i in range(2,500):
+            t=0
+            for j in range(2,i):
+                if i%j==0:
+                    t=1
+                    #time.sleep(1)
+            if t==0:
+                print("prime",i)
                 #time.sleep(1)
-        if t==0:
-            print("prime",i)
-            #time.sleep(1)
-def palindrome():
-    for i in range(2,500):
-        a=str(i)
-        if a==a[::-1]:
-            print("palindrome",i)
-            
-if __name__ == "__main__":
-    t1=multiprocessing.Process(target=prime)
-    t2=multiprocessing.Process(target=palindrome)
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
+        logging.info("prime is completed")
+    def palindrome():
+        for i in range(2,500):
+            a=str(i)
+            if a==a[::-1]:
+                print("palindrome",i)
+        logging.info("plaindrome completed")
+                
+    if __name__ == "__main__":
+        t1=multiprocessing.Process(target=prime)
+        t2=multiprocessing.Process(target=palindrome)
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+except:
+    logging.error("please check the program")
